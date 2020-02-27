76b3ed7f47244326a32cc8bfc6ba2461480da565
WalterKeiler <61295061+WalterKeiler@users.noreply.github.com>
2020-02-27 15:56:16 -0500

    Code examples for tank game
    This script is for rotating the barrels and turrets allowing for aim.

---
+++ BarrelRotate.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelRotate : MonoBehaviour
+{
+    public int m_PlayerNumber = 1;
+    public float scroll;
+    public float scrollSpeed = 50.0f;
+    public float top = 330f;
+    public float bottom = 355f;
+
+
+    void Start()
+    {
+        scroll = Input.GetAxis("Scroll" + m_PlayerNumber);
+    }
+
+    void Update()
+    {
+
+        if (m_PlayerNumber <= 1)
+        {
+            scroll = Input.GetAxis("Scroll1");
+            transform.Rotate(Vector3.left * scroll * 1);
+        }
+        else
+        {
+            scroll = Input.GetAxis("Scroll2");
+            transform.Rotate(Vector3.left * scroll * scrollSpeed);
+        }
+
+        bool InBetween = false;
+        Vector3 currentRotation = transform.localRotation.eulerAngles;
+        //Debug.Log("CURRENT ROTATION: " + currentRotation.ToString());
+
+        // Because this is wrapping rotation, I have 2 positive values
+        if (currentRotation.x > top && currentRotation.x <= 360.0f || currentRotation.x >= 360 && currentRotation.x <= bottom)
+        {
+            InBetween = true;
+        }
+        else
+        {
+            InBetween = false;
+        }
+        // if its outside the range you want, force it back to the closest value within the range and set the rotation via transform.localRotation
+        if (!InBetween)
+        {
+            // Closer to 40
+            if (Mathf.Abs(currentRotation.x - top) > Mathf.Abs(currentRotation.x - 40))
+            {
+                currentRotation.x = bottom;
+                transform.localRotation = Quaternion.Euler(currentRotation);
+                InBetween = true;
+            }
+            // Closer to 320
+            else if (Mathf.Abs(currentRotation.x - bottom) > Mathf.Abs(currentRotation.x - top))
+            {
+                currentRotation.x = top;
+                transform.localRotation = Quaternion.Euler(currentRotation);
+                InBetween = true;
+            }
+        }
+    }
+}
+
+
---
+++ TurretRotate.cs
@@ -0,0 +1,28 @@
+xusing System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretRotate : MonoBehaviour
+{
+    public float scroll;
+    public float scrollSpeed = 50.0f;
+    public int m_PlayerNumber = 1;
+
+    void FixedUpdate()
+    {
+        if (m_PlayerNumber >= 2)
+        {
+            var mouse = Input.mousePosition;
+            var screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+            var offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
+            var angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, -angle + 145, 0);
+        }
+
+        else
+        {
+            scroll = Input.GetAxis("mousePosition2");
+            transform.Rotate(Vector3.up * scroll * 20);
+        }
+    }
+}
